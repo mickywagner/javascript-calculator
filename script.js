a658ec7862b81d42a81c0ccf54d349740ee98242
Michaela Wagner <mickywagner@gmail.com>
2019-07-25 17:51:57 -0600

    Create operate switch statement and event listener for operatorBtns

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Math Functions
+
 function add(a, b) {
     return a + b
 }
@@ -17,18 +19,36 @@ function divide(a, b) {
     return a / b
 }
 
-function operate(operator, num1, num2) {
-    add(num1, num2)
+function operate(operator, input1, input2) {
+    switch (operator) {
+        case 'add':
+            return displayDiv.innerHTML = add(input1, input2)
+            break;
+        case 'subtract':
+            return displayDiv.innerHTML = subtract(input1, input2)
+            break;
+        case 'multiply':
+            return displayDiv.innerHTML = multiply(input1, input2)
+            break;
+        case 'divide':
+            return displayDiv.innerHTML = divide(input1, input2)
+            break;
+    }
 }
+
+// Button Events and Functions
 
 let displayDiv = document.querySelector('#display')
 let displayValue; 
+let input1;
+let input2;
+let operator;
 
 let numBtns = document.querySelectorAll('.num')
 numBtns.forEach(button => button.addEventListener('click', numToDisplay))
 
 function numToDisplay(e) {
-    displayValue = displayDiv.innerHTML = displayDiv.innerHTML + e.target.id
+    displayValue = displayDiv.innerHTML = displayDiv.innerHTML + e.target.id 
 }
 
 let clear = document.querySelector('#clear')
@@ -36,3 +56,13 @@ clear.addEventListener('click', () => {
     displayValue = ""
     displayDiv.innerHTML = displayValue
 })
+
+let operatorBtns = document.querySelectorAll('.operator')
+operatorBtns.forEach(button => button.addEventListener('click', (e) => {
+    operator = e.target.id
+    console.log(operator)
+    input1 = displayValue
+    console.log(input1)
+}))
+
+
